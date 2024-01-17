128278e1692d59028e728013c63de146669f4afe
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2024-01-17 14:58:54 +0100

    feat: initial commit

--- src/main/java/com/ingram/DecimalToRomanConverter.java
+++ src/main/java/com/ingram/DecimalToRomanConverter.java
@@ -3,41 +3,7 @@ package com.ingram;
 public class DecimalToRomanConverter {
 
     public static String toRoman(int decimal) {
-        StringBuilder roman = new StringBuilder();
 
-        for (RomanToDecimal romanToDecimal : RomanToDecimal.values()) {
-            while (decimal >= romanToDecimal.decimal) {
-                roman.append(romanToDecimal.roman);
-                decimal -= romanToDecimal.decimal;
-            }
-        }
-
-        roman.append("I".repeat(Math.max(0, decimal)));
-        return roman.toString();
-    }
-
-    enum RomanToDecimal {
-        THOUSAND(1000,"M"),
-        NINE_HUNDRED(900,"CM"),
-        FIVE_HUNDRED(500,"D"),
-        FOUR_HUNDRED(400,"XD"),
-        HUNDRED(100,"C"),
-        NINETY(90,"XC"),
-        FIFTY(50, "L"),
-        FORTY(40, "XL"),
-        TEN(10, "X"),
-        NINE(9, "IX"),
-        FIVE(5, "V"),
-        FOUR(4, "IV"),
-        ONE(1, "I");
-
-        private final int decimal;
-        private final String roman;
-
-        RomanToDecimal(int decimal, String roman) {
-
-            this.decimal = decimal;
-            this.roman = roman;
-        }
+        return "";
     }
 }
--- src/test/java/DecimalToRomanConverterShould.java
+++ src/test/java/DecimalToRomanConverterShould.java
@@ -1,30 +1,9 @@
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
-import static com.ingram.DecimalToRomanConverter.toRoman;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class DecimalToRomanConverterShould {
 
-    @ParameterizedTest
-    @CsvSource({
-            "1,I",
-            "2,II",
-            "3,III",
-            "5,V",
-            "6,VI",
-            "8,VIII",
-            "10,X",
-            "11,XI",
-            "17,XVII",
-            "19,XIX",
-            "22,XXII",
-            "298,CCXCVIII",
-            "299,CCXCIX",
-            "300,CCC",
-            "1983,MCMLXXXIII"
-    })
-    void convertDecimalToRoman(int input, String expected) {
-        assertThat(toRoman(input)).isEqualTo(expected);
+
+    void convertDecimalToRoman() {
+        assertThat("1").isEqualTo("I");
     }
 }
